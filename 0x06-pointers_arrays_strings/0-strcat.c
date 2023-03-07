512e154a661f42b2e58a5d90e419ad3d02c26dc8
Kiprotich <plkiprotich@gmail.com>
2023-03-06 22:47:59 -0800

    0-memset.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,21 @@
 #include "main.h"
-/**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
- */
-char *_strcat(char *dest, char *src)
-{
-	int i;
-	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
+/**
+ * _memset-fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	
+	for (; n > 0; i++)
 	{
-		i++;
+		s[i] = b;
+		n--;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		desr[i] = src[i];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (s);
 }
